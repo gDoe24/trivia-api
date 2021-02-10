f8bc20f38863ea96475caa0e2353fdc2780fd3b2
gDoe24 <gregkeel11@gmail.com>
2021-02-10 18:41:59 -0500

    manage.py to point to flaskr/models

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from backend.flaskr import create_app
-from backend.models import db
+from backend.flaskr.models import db
 
 migrate = Migrate(create_app(), db)
 manager = Manager(create_app())
